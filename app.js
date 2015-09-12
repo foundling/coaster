79b8e53b14d2dbb8276545f35b1a6013a340faae
Alex Ramsdell <foundling@users.noreply.github.com>
2015-09-12 00:24:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var Work = require('./work');
 
 var app = new Work();
 
-# should allow developer to omit GET argument, aka argv len 4 okay.
+// should allow developer to omit GET argument, aka argv len 4 okay.
 app.define('/', 'GET', function(req, res){
     res.writeHead(200, {
         'Content-type':'text/plain'
