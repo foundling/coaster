5e5124ff36e228b5c93887810d72aab65ab52b65
phylp <philson3@gmail.com>
2015-09-12 15:57:26 -0700

    first commit

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 var Work = require('./work');
 var app = new Work();
-app.ext.urlValidate = require('url-validate'); // behind the scenes validation
+//app.ext.urlValidate = require('url-validate'); // behind the scenes validation
 var port = process.argv[3] || 4000;
 
 
+// app.myConfig['head'] = 202;
+// app.myConfig['mime'] = {'Content-Type':'application/JSON'};
 
+app.config('templates', '/home/phylp/Desktop/framework/templates');
 
 // should allow developer to omit GET argument, aka 2 params okay.
-app.define('/', 'GET', function(req, res){
-
+app.define('/test', 'GET', function(req, res){
     // let developer define app configs like header values elsewhere with a config object, but the in-callback writeHead call can overwrite the config.
-    res.say('hi');
-    app.render('index'); // template engine
-    res.conclude('bye!');
+    //res.say('hi');
+    app.say(req, res, 'testing');
+    console.log('hey the test path works')
+    // app.render(req,res,'index.html'); // template engine
+    //res.conclude('bye!');
 });
+
+app.define('/other', 'GET', function(req, res){
+    //app.say(req, res, 'what else can we do?');
+    var obj = {
+                  'name':'bob',
+                  'age' : 10
+                };
+    app.render(res, 'index.html', obj);
+});
+
 
 app.define('/login', 'POST', function(req, res){
 
@@ -25,3 +39,14 @@ app.define('/login', 'POST', function(req, res){
 
 app.listen(4000);
 
+
+
+
+/* NOTES
+- not sure if conclude can be implemented because res.end is
+necessary for other helper functions to work
+
+-how do we render a view? No native method available in Node
+
+-what is purpose of say is we are rendering a view?
+*/
--- work.js
+++ work.js
@@ -1,13 +1,19 @@
 var http = require('http');
+var fs = require('fs');
+var _ = require('underscore');
 
 
-var Work  = function() {
+var Work  = function() {   //server will reference this map to handle request
     this.httpMethods = {
-        'GET' : {},
-        'POST' : {},
+      'GET' : {},
+      'POST' : {},
     };
+
+    this.params = {};
+    
     this.port = null;
-};
+    };
+
 
 Work.prototype.define = function(route, httpMethod, callback) {
     this.httpMethods[httpMethod][route] = callback;
@@ -18,14 +24,38 @@ Work.prototype.listen = function(port, callback) {
     this.port = port;
     var that = this;
     this.server = http.createServer(function(req, res) {
-        that.httpMethods[req.method][req.url](req,res);
+        that.httpMethods[req.method][req.url](req, res);  //executes request hander (httpMethods) upon receiving a req object from client 
+        //console.log(that.httpMethods);
     }).listen(this.port);
 
 };
 
-Work.prototype.config = function(config_param, value) {
+Work.prototype.say = function(req, res, val){
+  res.writeHead(this.params['head'], this.params['mime'])
+    // res.writeHead(this.params['head'], this.params['mime']  
+    //   || 200, {'Content-Type': 'text/plain'});
+    console.log("wrote "+ this.params['head']);
+    res.write('yo i invoked say');
+    res.end();
+};
 
-    
+Work.prototype.render = function(res,template_name, user){
+    var test =  fs.readFileSync(this.params['templates'] + '/' + template_name);
+      //function(err, data){
+      // if(err){
+      //   console.log(err);
+      // } else {
+        var tpl = _.template(test.toString());
+        var compiled_template = tpl(user);
+        res.write(compiled_template);
+        res.end();
+      }
+      
+
+Work.prototype.config = function(config_param, value) {
+    this.config_param = config_param;
+    this.params[config_param] = value;
+    console.log(this.params);
 };
 
 
