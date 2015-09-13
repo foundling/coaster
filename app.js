3ed16e16dcfefb0bb6c63771a628796482d9c626
Alex Ramsdell <alexramsdell@gmail.com>
2015-09-12 17:02:52 -0700

    cleaned up work.js files

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 var Work = require('./work');
 var app = new Work();
 //app.ext.urlValidate = require('url-validate'); // behind the scenes validation
-var port = process.argv[3] || 4000;
+var port = process.argv[2] || 4000;
 
-
-// app.myConfig['head'] = 202;
-// app.myConfig['mime'] = {'Content-Type':'application/JSON'};
 
 app.config('templates', '/home/phylp/Desktop/framework/templates');
 
 // should allow developer to omit GET argument, aka 2 params okay.
 app.define('/test', 'GET', function(req, res){
     // let developer define app configs like header values elsewhere with a config object, but the in-callback writeHead call can overwrite the config.
-    //res.say('hi');
     app.say(req, res, 'testing');
-    console.log('hey the test path works')
-    // app.render(req,res,'index.html'); // template engine
-    //res.conclude('bye!');
+    console.log('hey the test path works');
+});
+app.define('/', 'GET', function(req, res){
+    // let developer define app configs like header values elsewhere with a config object, but the in-callback writeHead call can overwrite the config.
+    app.say(req, res, 'testing');
+    console.log('hey the test path works');
 });
 
+app.define('/favicon.ico', 'GET', function(req, res){
+    // let developer define app configs like header values elsewhere with a config object, but the in-callback writeHead call can overwrite the config.
+    app.say(req, res, 'favicon!');
+    console.log('hey the favicon path works');
+});
+
+
+
 app.define('/other', 'GET', function(req, res){
-    //app.say(req, res, 'what else can we do?');
     var obj = {
                   'name':'bob',
                   'age' : 10
@@ -38,15 +44,3 @@ app.define('/login', 'POST', function(req, res){
 });
 
 app.listen(4000);
-
-
-
-
-/* NOTES
-- not sure if conclude can be implemented because res.end is
-necessary for other helper functions to work
-
--how do we render a view? No native method available in Node
-
--what is purpose of say is we are rendering a view?
-*/
--- work.js
+++ work.js
@@ -3,20 +3,35 @@ var fs = require('fs');
 var _ = require('underscore');
 
 
-var Work  = function() {   //server will reference this map to handle request
+var Work  = function() {   
+    //server references httpMethods to handle request
     this.httpMethods = {
-      'GET' : {},
-      'POST' : {},
+      'GET'     : {},
+      'POST'    : {},
+      'PUT'     : {},
+      'PATCH'   : {},
+      'DELETE'  : {},
     };
-
-    this.params = {};
-    
+    this._config = {
+        'path' : {
+            'templates': 'templates',
+        },
+        'encoding' : 'utf-8',
+        'mime' : {
+            'Content-type':'text/html'
+        },
+        'head':'',
+    };
     this.port = null;
-    };
 
+};
 
 Work.prototype.define = function(route, httpMethod, callback) {
-    this.httpMethods[httpMethod][route] = callback;
+    if (!httpMethod) {
+        this.httpMethods['GET'][route] = callback;
+    } else {
+        this.httpMethods[httpMethod][route] = callback;
+    }
 };
 
 Work.prototype.listen = function(port, callback) {
@@ -24,38 +39,44 @@ Work.prototype.listen = function(port, callback) {
     this.port = port;
     var that = this;
     this.server = http.createServer(function(req, res) {
-        that.httpMethods[req.method][req.url](req, res);  //executes request hander (httpMethods) upon receiving a req object from client 
+        that.httpMethods[req.method][req.url](req, res);  
+        //executes request hander (httpMethods) upon receiving a req object from client 
         //console.log(that.httpMethods);
     }).listen(this.port);
 
 };
 
 Work.prototype.say = function(req, res, val){
-  res.writeHead(this.params['head'], this.params['mime'])
-    // res.writeHead(this.params['head'], this.params['mime']  
-    //   || 200, {'Content-Type': 'text/plain'});
-    console.log("wrote "+ this.params['head']);
+    res.writeHead(200, this._config['mime']);
+    console.log("wrote "+ this._config['head']);
     res.write('yo i invoked say');
     res.end();
 };
 
 Work.prototype.render = function(res,template_name, user){
-    var test =  fs.readFileSync(this.params['templates'] + '/' + template_name);
-      //function(err, data){
-      // if(err){
-      //   console.log(err);
-      // } else {
-        var tpl = _.template(test.toString());
-        var compiled_template = tpl(user);
-        res.write(compiled_template);
-        res.end();
-      }
-      
+
+    var template_path = [
+        this._config['path']['templates'], 
+        '/',
+        template_name,
+    ]
+    .join('');
+
+    fs.readFile(template_path, this._config['encoding'], function(err, data) {
+        // actual error handling should go here;
+        if (err) console.log(err);
+        else {
+            var tpl = _.template(data); 
+            var compiled_template = tpl(user);
+            res.write(compiled_template);
+            res.end();
+        }
+    }); 
+};
 
 Work.prototype.config = function(config_param, value) {
-    this.config_param = config_param;
-    this.params[config_param] = value;
-    console.log(this.params);
+    this._config[config_param] = value;
+    console.log(this._config);
 };
 
 
