e3c6d8fe404b7e1523831f73efefddc53da3cd08
Alex Ramsdell <alexramsdell@gmail.com>
2015-09-14 09:00:48 -0700

    added render to app.js demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // demo file
 
-var Work = require('./lib/work');
-var app = new Work();
+var Coaster = require('./lib/work');
+var app = new Coaster();
 var port = process.argv[2] || 5000;
 
 // Defaults to GET request
@@ -9,40 +9,54 @@ app.define('/test', function(req, res) {
     res.send('This is a test of /test');
 });
 
+// Explicit GET request
 app.define('/', 'GET', function(req, res) {
     res.send('/ test with res.send');
-
 });
 
-app.define('/user', 'GET', function(req, res) {
-    var person = {
-        'name' : 'bob',
-        'id' : '3045'
-    };
-    res.sendJSON(person);
+
+// GET Request with Rendered Template
+app.define('/data', 'GET', function(req, res) {
+    app.render(res, 'index.html', {'name':'Alex'});
 });
 
+// GET Request with a JSON response
 app.define('/alex', 'GET', function(req, res) {
     res.sendJSON('{"name":"alex"}');
 });
 
-app.define('/post_test', 'POST', function(req, res) {
+// handling a post request 
+app.define('/msg_len', 'POST', function(req, res) {
 
     res.send('received your post request, but didnt look at the data yet');
+    console.log(typeof req.body);
 });
 
-app.define('/post_test', 'POST', function(req, res) {
-    var out = '';
-    req.on('data', function(chunk) {
-        out += chunk; 
+app.define('/signin', 'POST', function(req, res) {
+
+    var data = 0,
+        that;
+
+    req.on('data', function(data) {
+        data += data; 
     });
-    req.on('end', function() {
-        res.send(out);
+
+    req.on('end', function(chunk) {
+        // validate user credentials 
+        that.send('signin data received'); 
     });
-    console.log(req.body);
 }); 
 
-app.listen(port, function() {
-    console.log('CONFIG VALUES:');
-    console.log(JSON.stringify(app._config, undefined, 2));
-});
+function onRequest() {
+    console.log('\n\n\n\n\n\nCOASTER\n~~~~~~~~~~\n\n\n\n\n' +
+                'Running on port ' + 
+                  port +
+                ' with Config Values:' +
+                '\n'
+    );
+
+    // print to console, avoid a library
+    console.log(JSON.stringify(app._config, undefined, 2),'\n');
+}
+
+app.listen(port, onRequest);
